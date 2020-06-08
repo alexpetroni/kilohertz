98b942ba328f8de825a92e888bf67972e49137b5
alexpetroni <alexpetroni@gmail.com>
2020-06-08 11:39:36 +0200

    Added material theme to front

--- front/src/main.js
+++ front/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import { createProvider } from './vue-apollo'
+import './plugins/base'
 
 Vue.config.productionTip = false
 
