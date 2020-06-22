e5ea070ed25cae7bdf538a23310007d96b6410b4
alexpetroni <alexpetroni@gmail.com>
2020-06-22 13:21:28 +0200

    fix download

--- admin/src/views/settings/Export.vue
+++ admin/src/views/settings/Export.vue
@@ -11,11 +11,9 @@
       </v-col>
 
       <v-col cols="12">
-        <v-btn color="primary" @click="onExport">Export all products</v-btn>
+        <v-btn color="primary" :href="exportLink()" download>Export all products</v-btn>
       </v-col>
       <v-col cols="12">
-        <v-progress-circular v-if="loading" :value="40" indeterminate color="primary"></v-progress-circular>
-        <a v-if="downloadLink" :href="downloadLink" download>Download exported products file</a>
       </v-col>
     </v-row>
   </v-container>
@@ -25,36 +23,9 @@
 import ErrorMixin from '@common/mixins/ErrorMixin'
 import axios from 'axios'
 
-const productsExportUrl = process.env.VUE_APP_HTTP + process.env.VUE_APP_PATH_PRODUCTS_EXPORT
-
 export default {
-  name: '',
-
-  components: {
-
-  },
-
-  directives: {
-
-  },
-
-  filters: {
-
-  },
-
-  extends: {
-
-  },
-
   mixins: [ ErrorMixin ],
 
-  model: {
-
-  },
-
-  props: {
-
-  },
 
   data () {
     return {
@@ -63,32 +34,23 @@ export default {
     }
   },
 
-  computed: {
-
-  },
-
-  watch: {
-
-  },
-
   methods: {
     onExport () {
       this.loading = true
       axios
-      .get(productsExportUrl)
+      .get('/hh')
       .then(result => {
         this.downloadLink = result.data
         this.loading = false
         })
       .catch(error => this.setError(error.message))
-    }
+    },
+
+    exportLink () {
+      return process.env.VUE_APP_HTTP + process.env.VUE_APP_PATH_PRODUCTS_EXPORT
+    },
   },
 
 
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
